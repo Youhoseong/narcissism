4cdeebf50d5706c89f70781ba0d1d3a3c4ae495a
Youhoseong <h0song@naver.com>
2021-02-07 21:03:24 +0900

    delete chat system

--- purchases/views.py
+++ purchases/views.py
@@ -1,10 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
 from django.views.generic import ListView
 from . import models
 from ipware import get_client_ip
 from django.contrib.gis.geoip2 import GeoIP2
 from django.shortcuts import render, HttpResponse
 import os
+
+
+import requests
 # Create your views here.
 
 
@@ -13,24 +16,24 @@ class HomeView(ListView):
     model = models.Purchase
     template_name = "home.html"
     context_object_name = "purchases"
-   
-    
+  
     def get(self, request):
+        # response = requests.get('https://ncloud.apigw.ntruss.com/geolocation/v1').json()
+        # print(response)
         x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
-        client_id = os.environ.get("CLIENT_ID_NAVER_MAP")
+        client_id = os.environ.get("KAKAO_MAP_KEY")
         print(client_id)
         if x_forwarded_for:
             ip = x_forwarded_for.split(',')[0]
         else:
             ip = request.META.get('REMOTE_ADDR')
 
-            g = GeoIP2()
-            print(g.country('naver.com'))
-        
+        g = GeoIP2()
+        print(g.city('218.146.29.228'))
 
         return render(request, "home.html", {
             "ip": ip,
-            "client_id_naver": client_id,
+            "client_id_kakao": client_id,
         }) 
 
 # FBV home definition 이해를 위해 남겨둠.
