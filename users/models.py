e45138f237d2753fcf5cb8261d8998e64e7103c5
heoboseong7 <boseong844@gmail.com>
2021-02-09 18:04:25 +0900

    Add Sign up and login

--- users/models.py
+++ users/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+
 # Create your models here.
 
 
@@ -8,10 +9,7 @@ class User(AbstractUser):
     GENDER_FEMALE = "female"
     GENDER_OTHER = "other"
 
-    GENDER_CHOICES = (
-        (GENDER_MALE, "Male"),
-        (GENDER_FEMALE, "Female"),
-    )
+    GENDER_CHOICES = ((GENDER_MALE, "Male"), (GENDER_FEMALE, "Female"))
 
     LOGIN_EMAIL = "email"
     LOGIN_GITHUB = "github"
@@ -20,12 +18,10 @@ class User(AbstractUser):
     LOGIN_CHOICES = (
         (LOGIN_EMAIL, "Email"),
         (LOGIN_GITHUB, "Github"),
-        (LOGIN_KAKAO, "Kakao")
+        (LOGIN_KAKAO, "Kakao"),
     )
-    gender = models.CharField(
-        choices=GENDER_CHOICES, max_length=10, blank=True
-    )
-
+    gender = models.CharField(choices=GENDER_CHOICES, max_length=10, blank=True)
+    email = models.EmailField(unique=True)
     avatar = models.FileField(upload_to="avatars", blank=True)
 
     bio = models.TextField(default="", blank=True)
@@ -34,4 +30,6 @@ class User(AbstractUser):
     address = models.CharField(max_length=80, blank=True)
 
     email_verified = models.BooleanField(default=False)
-    login_method = models.CharField(max_length=50, choices=LOGIN_CHOICES, default=LOGIN_EMAIL)
+    login_method = models.CharField(
+        max_length=50, choices=LOGIN_CHOICES, default=LOGIN_EMAIL
+    )
